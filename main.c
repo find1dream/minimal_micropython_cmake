fdd23133f000c8448dbfeb4dc2cb36ef48de9c1f
clinfeng <clinfeng.jlu@gmail.com>
2022-12-29 03:20:48 +0000

    update

--- main.c
+++ main.c
@@ -2,59 +2,37 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "py/builtin.h"
 #include "py/compile.h"
-#include "py/runtime.h"
-#include "py/repl.h"
 #include "py/gc.h"
 #include "py/mperrno.h"
+#include "py/stackctrl.h"
+#include "py/builtin.h"
+#include "py/runtime.h"
+#include "py/repl.h"
 #include "shared/runtime/pyexec.h"
-
-void do_str(const char *src, mp_parse_input_kind_t input_kind) {
-    nlr_buf_t nlr;
-    if (nlr_push(&nlr) == 0) {
-        mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, src, strlen(src), 0);
-        qstr source_name = lex->source_name;
-        mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
-        mp_obj_t module_fun = mp_compile(&parse_tree, source_name, true);
-        mp_call_function_0(module_fun);
-        nlr_pop();
-    } else {
-        // uncaught exception
-        mp_obj_print_exception(&mp_plat_print, (mp_obj_t)nlr.ret_val);
-    }
-}
+#include "shared/runtime/gchelper.h"
 
 static char *stack_top;
-static char heap[2048];
+static char heap[4096];
 
 int main(int argc, char **argv) {
-    int stack_dummy;
-    stack_top = (char *)&stack_dummy;
-
-    #if MICROPY_ENABLE_GC
+    // Initialise the MicroPython runtime.
+    mp_stack_ctrl_init();
     gc_init(heap, heap + sizeof(heap));
-    #endif
     mp_init();
-    #if MICROPY_ENABLE_COMPILER
-    #if MICROPY_REPL_EVENT_DRIVEN
-    pyexec_event_repl_init();
-    for (;;) {
-        int c = mp_hal_stdin_rx_chr();
-        if (pyexec_event_repl_process_char(c)) {
-            break;
-        }
-    }
-    #else
+
+    // Start a normal REPL; will exit when ctrl-D is entered on a blank line.
     pyexec_friendly_repl();
-    #endif
-    // do_str("print('hello world!', list(x+1 for x in range(10)), end='eol\\n')", MP_PARSE_SINGLE_INPUT);
-    // do_str("for i in range(10):\r\n  print(i)", MP_PARSE_FILE_INPUT);
-    #else
-    pyexec_frozen_module("frozentest.py");
-    #endif
+
+    // Deinitialise the runtime.
+    gc_sweep_all();
     mp_deinit();
     return 0;
+}
+// Handle uncaught exceptions (should never be reached in a correct C implementation).
+void nlr_jump_fail(void *val) {
+    for (;;) {
+    }
 }
 
 void gc_collect(void) {
@@ -67,29 +45,12 @@ void gc_collect(void) {
     gc_dump_info();
 }
 
-mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
-    mp_raise_OSError(MP_ENOENT);
-}
-
+// There is no filesystem so stat'ing returns nothing.
 mp_import_stat_t mp_import_stat(const char *path) {
     return MP_IMPORT_STAT_NO_EXIST;
 }
 
-void nlr_jump_fail(void *val) {
-    while (1) {
-        ;
-    }
+// There is no filesystem so opening a file raises an exception.
+mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
+    mp_raise_OSError(MP_ENOENT);
 }
-
-void NORETURN __fatal_error(const char *msg) {
-    while (1) {
-        ;
-    }
-}
-
-#ifndef NDEBUG
-void MP_WEAK __assert_func(const char *file, int line, const char *func, const char *expr) {
-    printf("Assertion '%s' failed, at file %s:%d\n", expr, file, line);
-    __fatal_error("Assertion failed");
-}
-#endif
--- mpconfigport.h
+++ mpconfigport.h
@@ -3,12 +3,10 @@
 // options to control how MicroPython is built
 
 // Use the minimal starting configuration (disables all optional features).
-#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_MINIMUM)
+#define MICROPY_CONFIG_ROM_LEVEL          (MICROPY_CONFIG_ROM_LEVEL_MINIMUM)
 
-// You can disable the built-in MicroPython compiler by setting the following
-// config option to 0.  If you do this then you won't get a REPL prompt, but you
-// will still be able to execute pre-compiled scripts, compiled with mpy-cross.
 #define MICROPY_ENABLE_COMPILER           (1)
+#define MICROPY_ERROR_REPORTING           (MICROPY_ERROR_REPORTING_TERSE)
 
 //#define MICROPY_QSTR_EXTRA_POOL           mp_qstr_frozen_const_pool
 #define MICROPY_ENABLE_GC                 (1)
@@ -16,6 +14,15 @@
 
 #define MICROPY_ALLOC_PATH_MAX            (256)
 #define MICROPY_ALLOC_PARSE_CHUNK_INIT    (16)
+
+// Fine control over Python builtins, classes, modules, etc.
+#define MICROPY_PY_ASYNC_AWAIT            (0)
+#define MICROPY_PY_BUILTINS_SET           (0)
+#define MICROPY_PY_ATTRTUPLE              (0)
+#define MICROPY_PY_COLLECTIONS            (0)
+#define MICROPY_PY_MATH                   (0)
+#define MICROPY_PY_IO                     (0)
+#define MICROPY_PY_STRUCT                 (0)
 
 // type definitions for the specific machine
 
@@ -28,7 +35,5 @@ typedef long mp_off_t;
 
 #define MICROPY_HW_BOARD_NAME "minimal"
 #define MICROPY_HW_MCU_NAME "unknown-cpu"
-
-//#define MICROPY_MIN_USE_STDOUT (1)
 
 #define MP_STATE_PORT MP_STATE_VM
